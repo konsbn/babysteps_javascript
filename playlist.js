36b136f0e629c3564486f551bc9b0e949f6337cc
Shubham Bhushan <konsbn@outlook.com>
2013-06-26 02:16:38 +0530

    new file:   playlist.js

---
+++ playlist.js
@@ -0,0 +1,16 @@
+
+function init(){
+	var button = document.getElementById("addButton");
+	button.onclick = handleButtonClick;
+}
+function handleButtonClick(){
+	alert("button was clicked");
+}
+function test(){
+	var test = getElementById("test");
+	test.onclick = alertfn;
+}
+function alertfn(){
+	alert("it worked!";)
+}
+window.onload = init;
